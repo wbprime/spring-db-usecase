ce007dabad441e61af9f4ddc3e76ade9c635bd46
王勃 <bo.wang35@renren-inc.com>
2016-04-28 23:09:50 +0800

    Add springdata-jpa-xml sub project

---
+++ springdata-jpa-xml/src/main/java/me/wbprime/springdbusecase/springdatajpa/xml/dao/PingDAO.java
@@ -0,0 +1,21 @@
+package me.wbprime.springdbusecase.springdatajpa.xml.dao;
+
+
+import me.wbprime.springdbusecase.springdatajpa.xml.models.Ping;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+/**
+ * Interface: PingDAO
+ * Date: 2016/04/18 22:08
+ *
+ * @author Elvis Wang [mail@wbprime.me]
+ */
+public interface PingDAO extends JpaRepository<Ping, Integer> {
+    List<Ping> findAll();
+
+    Ping findOne(Integer id);
+
+    void delete(final Ping id);
+}
---
+++ springdata-jpa-xml/src/main/java/me/wbprime/springdbusecase/springdatajpa/xml/services/PingServiceI.java
@@ -0,0 +1,25 @@
+package me.wbprime.springdbusecase.springdatajpa.xml.services;
+
+
+import me.wbprime.springdbusecase.springdatajpa.xml.dto.PingDTO;
+import me.wbprime.springdbusecase.springdatajpa.xml.exceptions.PingNotFoundException;
+
+import java.util.List;
+
+/**
+ * Interface: PingServiceI
+ * Date: 2016/04/18 22:08
+ *
+ * @author Elvis Wang [mail@wbprime.me]
+ */
+public interface PingServiceI {
+    PingDTO insert(PingDTO dto);
+
+    List<PingDTO> findAll();
+
+    PingDTO findOne(int id) throws PingNotFoundException;
+
+    PingDTO updateOne(int id, PingDTO dto) throws PingNotFoundException;
+
+    PingDTO deleteOne(int id) throws PingNotFoundException;
+}
